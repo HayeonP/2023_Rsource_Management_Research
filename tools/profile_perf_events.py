cb72c0ef5f98958e7d629f8edb313e1c85ae8cb9
HayeonP <kite9240@gmail.com>
2023-09-27 15:28:53 +0900

    Detect log existence on profile_perf_events.sh

--- tools/profile_perf_events.py
+++ tools/profile_perf_events.py
@@ -5,13 +5,16 @@ import yaml
 if __name__ == '__main__':
     with open('configs/perf_events.yaml', 'r') as f:
         configs = yaml.safe_load(f)
-
-    os.system('mkdir -p perf_events_log')
     
     label = configs['label']
     duration = configs['duration']
     target_cores = configs['target_cores']
     target_events = configs['target_events']
+
+    os.system('mkdir -p perf_events_log')
+    if os.path.exists(f'perf_events_log/{label}.txt'):
+        print('[ERROR] Same name of log already exists.')
+        exit()
 
     target_events_str = ''
     for i, event in enumerate(target_events):
