61f23887c9645aeef24aa21eff08dd13810ab454
HoYongLee98 <dlghdydwkd79@gmail.com>
2023-10-11 16:48:12 +0900

    remove bandwidth analyzer from profiler

--- tools/profile_bandwidth.sh
+++ tools/profile_bandwidth.sh
@@ -6,4 +6,4 @@ ssh root@$HOSTNAME "mkdir -p /home/root/sdd/tools/bw_profiler"
 scp -r configs root@$HOSTNAME:/home/root/sdd/tools
 scp profile_bandwidth.py root@$HOSTNAME:/home/root/sdd/tools
 ssh root@$HOSTNAME "cd /home/root/sdd/tools && python3 profile_bandwidth.py"
-python3 analyze_bandwidth.py
+# python3 analyze_bandwidth.py
