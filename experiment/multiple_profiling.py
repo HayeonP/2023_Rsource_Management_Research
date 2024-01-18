84bb7b0b10f84b7912fa987ed7f89a1b63a3de1e
HayeonP <kite9240@gmail.com>
2024-01-18 11:54:10 +0900

    Add multiple profiling

---
+++ experiment/multiple_profiling.py
@@ -0,0 +1,34 @@
+import yaml
+import os
+
+version_list = [9, 10]
+repeat_list = [20, 20]
+
+for i in range(len(version_list)):
+    with open("yaml/svl_auto_experiment_configs.yaml", "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+        
+    version = version_list[i]
+    repeat = repeat_list[i]
+    config["experiment_title"] = config["experiment_title"].split("x")[0] + "x" + str(repeat) + "_v" + str(version)
+    config["max_iteration"] = repeat
+    
+    with open("yaml/svl_auto_experiment_configs.yaml", "w") as f:
+        yaml.dump(config, f)
+    
+    os.system("python3 svl_auto_experiment.py")
+    
+    with open("yaml/autoware_analyzer.yaml", "r") as f:
+        analyzer_config = yaml.load(f, Loader=yaml.FullLoader)
+    
+    analyzer_config["experiment_title"] = [config["experiment_title"]]
+    analyzer_config["output_title"] = [config["experiment_title"]]
+    
+    with open("yaml/autoware_analyzer.yaml", "w") as f:
+        yaml.dump(analyzer_config, f)
+        
+    os.system("python3 autoware_analyzer.py")
+    
+    
+    
+
